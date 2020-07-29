b8310a2395c5616b96660cd4ac52815718f6ab18
Camilo Jaramillo <camilok67@outlook.com>
2020-07-30 03:20:37 +0800

    Creacion del proyecto

---
+++ ExamenIngreso/Models/Producto.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ExamenIngreso.Models
+{
+    public class Producto
+    {
+        public string Nombre { get; set; }
+        public string SKU { get; set; }
+        public string Descripción { get; set; }
+        public string Valor { get; set; }
+        public string Tienda { get; set; }
+        public string Imagen { get; set; }
+
+    }
+}
---
+++ ExamenIngreso/Models/Tienda.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ExamenIngreso.Models
+{
+    public class Tienda
+    {
+        public int Id { get; set; }
+
+        public int Nombre { get; set; }
+        
+        public string FechaApertura { get; set; }
+    }
+}
