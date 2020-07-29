a53576933742d1fd9dd8b0c82a8235f175ce75a8
Camilo Jaramillo <camilok67@outlook.com>
2020-07-30 03:37:34 +0800

    Creacion de modelos

--- ExamenIngreso/Models/Producto.cs
+++ ExamenIngreso/Models/Producto.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -7,11 +8,27 @@ namespace ExamenIngreso.Models
 {
     public class Producto
     {
-        public string Nombre { get; set; }
+        [Required]
         public string SKU { get; set; }
+
+        [Display(Name = "Nombre")]
+        [StringLength(40, ErrorMessage = "El número de caracteres de {0} debe ser al menos {2}.", MinimumLength = 3)]
+        public string Nombre { get; set; }
+
+        [Display(Name = "Descripción")]
+        [StringLength(40, ErrorMessage = "El número de caracteres de {0} debe ser al menos {2}.", MinimumLength = 10)]
         public string Descripción { get; set; }
-        public string Valor { get; set; }
+
+        [Display(Name = "Valor")]
+        [Required(ErrorMessage = "Debe Ingresar el {0}")]
+        [DataType(DataType.Currency)]
+        public decimal Valor { get; set; }
+
+        [Display(Name = "Tienda")]
+        [StringLength(40, ErrorMessage = "El número de caracteres de {0} debe ser al menos {2}.", MinimumLength = 3)]
         public string Tienda { get; set; }
+
+        [Display(Name = "Imagen")]
         public string Imagen { get; set; }
 
     }
--- ExamenIngreso/Models/Tienda.cs
+++ ExamenIngreso/Models/Tienda.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -9,8 +10,12 @@ namespace ExamenIngreso.Models
     {
         public int Id { get; set; }
 
+        [Display(Name = "Nombre")]
+        [StringLength(40, ErrorMessage = "El número de caracteres de {0} debe ser al menos {2}.", MinimumLength = 3)]
         public int Nombre { get; set; }
-        
+
+        [Display(Name = "Fecha de Apertura")]
+        [DisplayFormat(DataFormatString = "{0:yyyy/MM/dd}", ApplyFormatInEditMode = true)]
         public string FechaApertura { get; set; }
     }
 }
